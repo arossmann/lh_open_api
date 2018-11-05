e89f430236a08eaf0e8ce100e42697620ee22e16
Arne Rossmann <arne.rossmann@capgemini.com>
2018-11-05 14:32:17 +0100

    renamed directory and package name

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='LH Open API',
       version='0.1',
       description='Python Wrapper for the Lufthansa Open API,
-      url='https://github.com/arossmann/LH_OpenAPI',
+      url='https://github.com/arossmann/lh_open_api',
       author='Arne Rossmann',
       author_email='arne.rossmann@gmail.com',
       license='MIT',
-      packages=['lh_openapi'],
+      packages=['lh_open_api'],
       zip_safe=False)
